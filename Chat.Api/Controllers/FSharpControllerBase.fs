cee8b3ccdb394090500e8b24ce7e593592d1f178
Kadi Gasanguseinov <22912194+lif0@users.noreply.github.com>
2020-05-07 16:24:37 +0300

    Update FSharpControllerBase.fs

--- Chat.Api/Controllers/FSharpControllerBase.fs
+++ Chat.Api/Controllers/FSharpControllerBase.fs
@@ -3,23 +3,6 @@
 open Microsoft.AspNetCore.Mvc
 open Chat
 
-[<AutoOpen>]
-module AAAA = 
-  let inline GetBodyAsync x = (^a: (member GetBodyAsync: unit -> ^b) x)
-  
-  type A() =
-      member this.GetBodyAsync() = System.Threading.Tasks.Task.FromResult 1
-  
-  type B() =
-      member this.GetBodyAsync() = async { return 2 }
-
-  let s() = 
-    let z = A() |> GetBodyAsync |> fun x -> x.Result
-    let z2 = B() |> GetBodyAsync |> Async.RunSynchronously
-
-    z
-  let inline ZZZZ x = (^a: (member Ok: unit -> OkResult) x)
-
 type FSharpControllerBase()=
   inherit Controller()
 
